f8a9f86fbc9fee83ed9a6f9bc290fa0e4e8da924
Dnyaneshwar Padmane <145318352+DnyaneshwarP12@users.noreply.github.com>
2023-10-16 07:15:50 +0530

    Add files via upload

---
+++ dayfourteen/interthreadcommunication/InterThreadCommunicationDemo.java
@@ -0,0 +1,20 @@
+//Program to demonstrate interThread communication
+package com.tns.dayfourteen.interthreadcommunication;
+
+public class InterThreadCommunicationDemo {
+	public static void main(String[] args) {
+		
+		//Shared Resource
+		Q obj=new Q();
+		
+		Producer p1=new Producer(obj); 
+		Consumer c1=new Consumer(obj); 
+					
+		try {
+			p1.join();
+			c1.join();
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+}
